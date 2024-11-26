4ab1d25f195a7acb60b55823b1943d1888255df8
Christian Popp <popp.christian88@gmail.com>
2024-11-26 21:03:09 +0200

    x

---
+++ react-project/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import useCurrencyInfo from './Components/API';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project/src/Components/API.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const APIKEY = "fca_live_C2su03vbd3I3vDXWwhHFqGQT92BskHNUYnoabFY0";
+const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${APIKEY}`;
+
+export default function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
+    useEffect(() => {
+      fetch(`apiUrl`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+    }, [currency])
+
+    console.log(data);
+    return data
+    
+}
+
